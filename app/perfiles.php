be56c340f81a71530d703de1506122625662b67c
AlejoHurtatis14 <neideralejandro.h@gmail.com>
2020-04-19 18:18:44 -0500

    Se organiza modelos de usuarios y perfiles.

--- app/perfiles.php
+++ app/perfiles.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class perfiles extends Model
 {
     //
-    public function permisos(){
+    public function usuarios(){
     /* return $this->belongsToMany('App\usuarios','usuarios_perfiles','fk_usuario','fk_perfil')->withPivot('estado', 'fk_perfil', 'fk_usuario'); */
     return $this->hasMany('App\usuarios', 'fk_perfil', 'id');
     }
--- app/usuarios.php
+++ app/usuarios.php
@@ -11,7 +11,7 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 class usuarios extends Model
 {
     //
-    public function permisos(){
+    public function perfil(){
         /* return $this->belongsToMany('App\perfiles','usuarios_perfiles','fk_usuario','fk_perfil')->withPivot('estado', 'fk_perfil', 'fk_usuario'); */
         return $this->belongsTo('App\perfiles', 'fk_perfil');
     }
